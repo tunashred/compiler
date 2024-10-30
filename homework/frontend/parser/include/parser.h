2466e8efb76ec773141870969135fb2bf823e54f
tunashred <enachealexandru.traian@gmail.com>
2024-10-30 20:29:45 +0200

    Update with progress

--- homework/frontend/parser/include/parser.h
+++ homework/frontend/parser/include/parser.h
@@ -3,9 +3,17 @@
 
 #include <stdbool.h>
 
+#include "lexer.h"
+
 bool consume(int code);
 
-bool baseType(int code);
+bool baseType(Token* tk);
+
+bool def_var(Token *tk);
+
+bool func_param(Token* tk);
+
+bool func_params(Token* tk);
 
 bool program();
 
--- homework/frontend/parser/src/parser.c
+++ homework/frontend/parser/src/parser.c
@@ -4,28 +4,82 @@
 #include <stdlib.h>
 
 #include "lexer.h"
+#include "utils.h"
 
 int    iTk;
 Token* consumed;
 
+// considering to change param to Token*
 bool consume(int code) {
     if (tokens[iTk].code == code) {
         consumed = &tokens[iTk++];
         return true;
     }
+    // not sure if this may happen. i will just keep it for the moment
+    printf("Consume: Code or token index do not match\n");
     return false;
 }
 
-bool baseType(int code) {
-    switch (code) {
+bool baseType(Token* tk) {
+    switch (tk->code) {
         case TYPE_INT:
         case TYPE_REAL:
         case TYPE_STR:
             return true;
         
         default:
-            return false;
+            err("Invalid variable type, at line %d", tk->line);
     }
+}
+
+bool def_var(Token *tk) {
+    if (tk->code == VAR) {
+        consume(tk->code);
+        if (tk->code == ID) {
+            consume(tk->code);
+            if (tk->code == COLON) {
+                if (baseType(tk)) {
+                    consume(tk->code);
+                    if (tk->code == SEMICOLON) {
+                        consume(tk->code);
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool func_param(Token* tk) {
+    if (tk->code == ID) {
+        consume(tk->code);
+        if (tk->code == COLON) {
+            consume(tk->code);
+            if (baseType(tk)) {
+                consume(tk->code);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool func_params(Token* tk) {
+    if (func_param(tk)) {
+        do {
+            if (tk->code != COMMA) {
+                break;
+            }
+            consume(tk->code);
+            if (!func_param(tk)) {
+                err("Invalid parameter definition, at line %d", tk->line);
+            }
+            consume(tk->code);
+        } while(1);
+    }
+    // means we accept functions with no params
+    return true;
 }
 
 // // program ::= ( defVar | defFunc | block )* FINISH
