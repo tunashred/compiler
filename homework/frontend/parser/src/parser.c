8f6d075b311cc10a5bc0868591d3a1c2d52b0674
tunashred <enachealexandru.traian@gmail.com>
2025-01-14 16:39:08 +0200

    Adding progress made

--- homework/frontend/parser/src/parser.c
+++ homework/frontend/parser/src/parser.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 
 #include "lexer.h"
+#include "semantic_analyzer/include/domain.h"
+#include "types_analyzer/include/types.h"
 #include "utils.h"
 
 /*  Ideas for improving error reporting
@@ -30,8 +32,15 @@ bool consume(int code) {
 bool baseType() {
     switch (tokens[iTk].code) {
         case TYPE_INT:
+            ret.type = TYPE_INT;
+            consume(tokens[iTk].code);
+            return true;
         case TYPE_REAL:
+            ret.type = TYPE_REAL;
+            consume(tokens[iTk].code);
+            return true;
         case TYPE_STR:
+            ret.type = TYPE_STR;
             consume(tokens[iTk].code);
             return true;
 
@@ -46,10 +55,18 @@ bool def_var() {
         if (!consume(ID)) {
             err("Variable declaration missing identifier, at line %d", tokens[iTk].line);
         }
+        const char* name = consumed->text;
+        Symbol* symbol = searchInCurrentDomain(name);
+        if (symbol) {
+            err("Symbol redefinition: %s, at line %d", name, tokens[iTk].line);
+        }
+        symbol = addSymbol(name, KIND_VAR);
+        symbol->local = crtFn != NULL;
         if (!consume(COLON)) {
             err("Variable declaration must contain ':' after identifier, at line %d", tokens[iTk].line);
         }
         if (baseType()) {
+            symbol->type = ret.type;
             if (consume(SEMICOLON)) {
                 return true;
             } else if (tokens[iTk].code == ASSIGN) {
@@ -108,6 +125,10 @@ bool expr_prefix() {
 bool expr_mul() {
     if (expr_prefix()) {
         while (consume(MUL) || consume(DIV)) {
+            Ret leftType = ret;
+            if (leftType.type == TYPE_STR) {
+                err("Operands of '*' or '/' expression cannot be string type, at line %d", tokens[iTk].line);
+            } 
             int last_code = tokens[iTk - 1].code;
             if (!expr_prefix()) {
                 if (last_code == MUL) {
@@ -115,6 +136,10 @@ bool expr_mul() {
                 }
                 err("Missing expression after operand '/', at line %d", tokens[iTk].line);
             }
+            if (leftType.type != ret.type) {
+                err("Different types between operands of '*' or '/' expression, at line %d", tokens[iTk].line);
+            }
+            ret.lval = false;
         }
         return true;
     }
@@ -125,12 +150,20 @@ bool expr_add() {
     if (expr_mul()) {
         while (consume(ADD) || consume(SUB)) {
             int last_code = tokens[iTk - 1].code;
+            Ret leftType = ret;
+            if (leftType.type == TYPE_STR) {
+                err("Operands of '+' or '-' expression cannot be string type, at line %d", tokens[iTk].line);
+            }
             if (!expr_mul()) {
                 if (last_code == ADD) {
                     err("Missing expression after operand '+', at line %d", tokens[iTk].line);
                 }
                 err("Missing expression after operand '-', at line %d", tokens[iTk].line);
             }
+            if (leftType.type != ret.type) {
+                err("Different types between operands of expression '+' or '-', at line %d", tokens[iTk].line);
+            }
+            ret.lval = false;
         }
         return true;
     }
@@ -141,9 +174,14 @@ bool expr_comp() {
     if (expr_add()) {
         // TODO: need to revise operator precedence table
         while (consume(EQUAL) || consume(NOT_EQ) || consume(LESS) || consume(GREATER) || consume(GREATER_EQ)) {
+            Ret leftType = ret;
             if (!expr_add()) {
                 err("Missing expression after comparation operand, at line %d", tokens[iTk].line);
             }
+            if (leftType.type != ret.type) {
+                err("Different types between operands at comparation expression, at line %d", tokens[iTk].line);
+            }
+            setRet(TYPE_INT, false);
         }
         return true;
     }
@@ -153,10 +191,22 @@ bool expr_comp() {
 bool expr_assign() {
     int start = iTk;
     if (consume(ID)) {
+        const char* name = consumed->text;
         if (consume(ASSIGN)) {
             if (!expr_comp()) {
                 err("Expected expression at assigment, at line %d", tokens[iTk].line);
             }
+            Symbol* symbol = searchSymbol(name);
+            if (!symbol) {
+                err("Undefined variable: %s, at line %d", name, tokens[iTk].line);
+            }
+            if (symbol->kind == KIND_FN) {
+                err("Function %s cannot be used as destination for assignment, at line %d", name, tokens[iTk].line);
+            }
+            if (symbol->type != ret.type) {
+                err("Source and destination must be same type at assignment expression, at line %d", tokens[iTk].line);
+            }
+            ret.lval = false;
             return true;
         }
     }
@@ -168,9 +218,17 @@ bool expr_assign() {
 bool expr_logic() {
     if (expr_assign()) {
         while (consume(AND) || consume(OR)) {
+            Ret leftType = ret;
+            if (leftType.type == TYPE_STR) {
+                err("The left operand of logic expression cannot be string, at line %d", tokens[iTk].line);
+            }
             if (!expr_assign()) {
                 err("Malformed expression logic, at line %d", tokens[iTk].line);
             }
+            if (ret.type == TYPE_STR) {
+                err("The right operand of logic expression cannot be string, at line %d", tokens[iTk].line);
+            }
+            setRet(TYPE_INT, false);
         }
         return true;
     }
@@ -199,6 +257,9 @@ bool instr() {
         if (!expr()) {
             err("If statement is not provided an expression, at line %d", tokens[iTk].line);
         }
+        if (ret.type == TYPE_STR) {
+            err("The if condition must have int or float, at line %d", tokens[iTk].line);
+        }
         if (!consume(RPAR)) {
             err("If statement must end with ')', at line %d", tokens[iTk].line);
         }
@@ -221,6 +282,12 @@ bool instr() {
         if (!expr()) {
             err("Expected expression at return, at line %d", tokens[iTk].line);
         }
+        if (!crtFn) {
+            err("Return can be used only inside a function, at line %d", tokens[iTk].line);
+        }
+        if (ret.type != crtFn->type) {
+            err("Return type does not match function type, at line %d", tokens[iTk].line);
+        }
         if (!consume(SEMICOLON)) {
             err("Return statement missing ';', at line %d", tokens[iTk].line);
         }
@@ -233,6 +300,9 @@ bool instr() {
         }
         if (!expr()) {
             err("Missing expression inside \"while\" statement, at line %d", tokens[iTk].line);
+        }
+        if (ret.type == TYPE_STR) {
+            err("The while condition must verify int or real values, at line %d", tokens[iTk].line);
         }
         if (!consume(RPAR)) {
             err("Missing right paranthesis at \"while\" statement, at line %d", tokens[iTk].line);
@@ -261,9 +331,21 @@ bool block() {
 }
 
 bool func_param() {
-    if (consume(ID) && consume(COLON) && baseType()) {
-        return true;
+    if (consume(ID)) {
+        const char* name = consumed->text;
+        Symbol* symbol = searchInCurrentDomain(name);
+        if (symbol) {
+            err("Function argument redefinition: %s, at line %d", name, tokens[iTk].line);
+        }
+        symbol = addSymbol(name, KIND_ARG);
+        Symbol* s_func_param = addFnArg(crtFn, name);
+        if (consume(COLON) && baseType()) {
+            symbol->type = ret.type;
+            s_func_param->type = ret.type;
+            return true;
+        }
     }
+
     err("Missing variable type at declaration, at line %d", tokens[iTk].line);
     return false;
 }
@@ -286,6 +368,14 @@ bool def_func() {
         if (!consume(ID)) {
             err("Function missing identifier, at line %d", tokens[iTk].line);
         }
+        const char* name = consumed->text;
+        Symbol* symbol = searchInCurrentDomain(name);
+        if (symbol) {
+            err("Function redefinition: %s, at line %d", name, tokens[iTk].line);
+        }
+        crtFn = addSymbol(name, KIND_FN);
+        crtFn->args = NULL;
+        addDomain();
         if (!consume(LPAR)) {
             err("Function declaration missing '(', at line %d", tokens[iTk].line);
         }
@@ -303,8 +393,13 @@ bool def_func() {
             err("Unknown function return type, at line %d", tokens[iTk].line);
         }
 
+        crtFn->type = ret.type;
+
         while (def_var()) {}
         block();
+
+        delDomain();
+        crtFn = NULL;
 
         if (consume(END)) {
             return true;
@@ -320,6 +415,8 @@ bool def_func() {
 }
 
 bool program() {
+    addDomain();
+    addPredefinedFns();
     def_func();
     for (;;) {
         if (def_var()) {
@@ -328,6 +425,7 @@ bool program() {
         } else
             break;
     }
+    delDomain();
     if (consume(FINISH)) {
         return true;
     } else
